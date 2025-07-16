9e6f9fe1edd8557268ae3404d4ff4817552efb5e
John Ayodeji <johnayodejie@gmail.com>
2025-07-16 13:56:24 +0100

    Sanitize user form input in authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -44,7 +44,6 @@ exports.signUp = catchAsync( async (req, res, next) => {
     const newUser = await User.create({
     name: req.body.name,
     email: req.body.email,
-    role: req.body.role,
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm
 });
